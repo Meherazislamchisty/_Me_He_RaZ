5be77b0a306b3cfbec6ae0c8f913ab0c07c27aef
meherazmeheraz <sharminara033@gmail.com>
2025-06-11 18:35:59 +0600

    Update and rename ayan.js to meheraz.js

--- scripts/cmds/meheraz.js
+++ scripts/cmds/meheraz.js
@@ -12,7 +12,7 @@ category: "no prefix",
 
 onStart: async function(){}, 
 onChat: async function({ event, message, getLang }) {
-if (event.body && event.body.toLowerCase() === "meheraz"" chisty") {
+if (event.body && event.body.toLowerCase() === "meheraz") {
 return message.reply({
 body: " ──────────◊\n‣ 𝐁𝐨𝐭 & 𝐎𝐰𝐧𝐞𝐫 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧  \n\n‣ 𝐍𝐚𝐦𝐞: 𝗠𝗘𝗛𝗘𝗥𝗔𝗭                           ‣ 𝐁𝐨𝐭 𝐍𝐚𝐦𝐞:𝐶ℎ𝑖𝑠𝑡𝑦'𝑠 𝐵𝑏'𝑧😘 ",
 attachment: await global.utils.getStreamFromURL("https://files.catbox.moe/7tfxcz.jpg")
