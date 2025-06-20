39bc61c1870b881f45a32de89efd07a87d2cd52f
gamingboymeheraz <gamingboymeheraz@gmail.com>
2025-06-20 22:17:15 +0600

    Update wife.js

--- scripts/cmds/wife.js
+++ scripts/cmds/wife.js
@@ -17,13 +17,13 @@ module.exports = {
       return message.reply({
         body:
           "╭─────────────╮\n" +
-          "  ʚ💗ɞ 𝐑𝐚𝐚𝐝'𝐬 𝐖𝐢𝐟𝐞𝐲 ɞ💗ɞ\n" +
+          "  ʚ💗ɞ Sabrina'𝐬 𝐖𝐢𝐟𝐞𝐲 ɞ💗ɞ\n" +
           "╰─────────────╯\n\n" +
           "✨ 𝑯𝒆𝒚! 𝑳𝒐𝒐𝒌 𝒘𝒉𝒐'𝒔 𝒉𝒆𝒓𝒆...\n" +
-          "𝐀𝐲𝐚𝐧'𝐬 𝐜𝐮𝐭𝐞 𝐥𝐢𝐭𝐭𝐥𝐞 𝐩𝐫𝐢𝐧𝐜𝐞𝐬𝐬 ❀\n\n" +
+          "MEHERAZ'𝐬 𝐜𝐮𝐭𝐞 𝐥𝐢𝐭𝐭𝐥𝐞 𝐩𝐫𝐢𝐧𝐜𝐞𝐬𝐬 ❀\n\n" +
           "───────⋆⋅☆⋅⋆───────\n" +
-          "『 𝓑𝓸𝓽 : 🕸️ SpideY 🕷️ 』",
-        attachment: await global.utils.getStreamFromURL("https://i.imgur.com/tPzzqVl.mp4")
+          "『 𝓑𝓸𝓽 :  』",
+        attachment: await global.utils.getStreamFromURL("https://files.catbox.moe/w8skxr.mp4")
       });
     }
   }
